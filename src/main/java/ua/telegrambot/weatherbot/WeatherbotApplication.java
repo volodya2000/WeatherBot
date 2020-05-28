f349d2b839cd4794d0bdbe99484b4fce9317bda5
volodya2000 <vovan.zhuk3.14@gmail.com>
2020-05-28 14:54:38 +0300

    adding first configuration of project

---
+++ src/main/java/ua/telegrambot/weatherbot/WeatherbotApplication.java
@@ -0,0 +1,13 @@
+package ua.telegrambot.weatherbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherbotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/telegrambot/weatherbot/WeatherbotApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.telegrambot.weatherbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherbotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
